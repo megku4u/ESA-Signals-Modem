2c1bf2a24b9bbc487d74872a7e6ae4d2c994bacd
Megan Ku <mku@olin.edu>
2020-05-04 17:13:18 -0400

    Applied cosine

--- modem_rx_starter.m
+++ modem_rx_starter.m
@@ -6,20 +6,67 @@ load short_modem_rx.mat
 % the transmission started. 
 
 start_idx = find_start_of_signal(y_r,x_sync);
+
 % start_idx now contains the location in y_r where x_sync begins
 % we need to offset by the length of x_sync to only include the signal
 % we are interested in
+
 y_t = y_r(start_idx+length(x_sync):end); % y_t is the signal which starts at the beginning of the transmission
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%  Put your decoder code here
 %%
+
+plot(y_t);
+% create time vector
+t = linspace(0, size(y_t,1)-1, size(y_t,1))';
+t = t./Fs;
+
+figure()
+DTFT_fftbased(y_t);
+
+%multiply cosine
+y_cos = y_t.*cos(2.*pi.*f_c.*t);
+
+figure()
+DTFT_fftbased(y_cos);
+
+% Apply low pass filter
+
+
+%% 
+
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 % convert to a string assuming that x_d is a vector of 1s and 0s
 % representing the decoded bits
-BitsToString(x_d)
+% BitsToString(x_d)
 
+%% FUNCTIONS
+function [X, O] = DTFT_fftbased(x)
+    % plots the magnitude of the Fourier transform of the signal x
+    % which is assumed to originate from a Continous-time signal 
+    % sampled with frequency fs
+    % the function returns X and f.
+    % In other words, this function plots the FT of the DT signal x
+    % with the frequency axis labeled as if it were the original CT signal
+    % 
+    % X contains the frequency response
+    % O contains the frequency samples 
+
+    N = length(x);
+
+    X = fftshift(fft(x));
+    O = linspace(-pi, pi - 2*pi/length(x), length(x));
+    subplot(211);
+    plot(O, abs(X));
+    xlabel('\Omega');
+    ylabel('|X(j\Omega)|');
+    subplot(212);
+    plot(O, unwrap(angle(X)));
+    xlabel('\Omega');
+    ylabel('\angle X(j\Omega)');
+    
+end
